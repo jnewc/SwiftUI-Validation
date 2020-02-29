56834662ebb170af02ea3b2073b6f770b22407d8
Jack Newcombe <jack@newcombe.io>
2020-02-29 16:52:55 +0000

    Initial Commit

---
+++ SwiftUI-Validation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Validation
+//
+//  Created by Jack Newcombe on 29/02/2020.
+//  Copyright © 2020 Jack Newcombe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-ValidationTests/SwiftUI_ValidationTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUI_ValidationTests.swift
+//  SwiftUI-ValidationTests
+//
+//  Created by Jack Newcombe on 29/02/2020.
+//  Copyright © 2020 Jack Newcombe. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftUI_Validation
+
+class SwiftUI_ValidationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
